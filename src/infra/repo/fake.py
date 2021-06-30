dd3e35dba93a8c7dcde50e643a8096d931337d2e
kaslufl <leallucasfonseca@gmail.com>
2021-06-30 20:42:14 -0300

    feat: add infra with config, entities, repo

---
+++ src/infra/repo/fake.py
@@ -0,0 +1,21 @@
+from src.infra.config import DBConnectionHandler
+from src.infra.entities import Users
+
+
+class FakerRepo:
+    """A Simple Repository"""
+
+    @classmethod
+    def insert_user(cls):
+        """Something"""
+
+        with DBConnectionHandler() as db_connection:
+            try:
+                new_user = Users(name="Lucas", password="Leal")
+                db_connection.session.add(new_user)
+                db_connection.session.commit()
+            except:
+                db_connection.session.rollback()
+                raise
+            finally:
+                db_connection.session.close()
